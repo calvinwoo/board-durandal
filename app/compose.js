21f324917445e8465a821369f208caeb665f3208
Calvin Woo <calvin.woo@citrix.com>
2014-07-09 10:00:41 -0700

    Basic version.

---
+++ app/compose.js
@@ -0,0 +1,5 @@
+define(function (require) {
+  var ko = require('knockout');
+
+  return {};
+});
---
+++ app/main.js
@@ -0,0 +1,29 @@
+requirejs.config({
+  paths: {
+    'text': '../lib/require/text',
+    'durandal':'../lib/durandal/js',
+    'plugins' : '../lib/durandal/js/plugins',
+    'transitions' : '../lib/durandal/js/transitions',
+    'knockout': '../lib/knockout/knockout-3.1.0',
+    'jquery': '../lib/jquery/jquery-1.9.1'
+  }
+});
+
+define(function (require) {
+  var system = require('durandal/system');
+  var app = require('durandal/app');
+
+  system.debug(true);
+ 
+  app.title = 'Durandal Message Board';
+   
+  app.configurePlugins({
+    router: true,
+    dialog: true
+  });
+
+  app.start().then(function() {
+    app.setRoot('shell');
+  });
+});
+ 
