abb77c87bdd678dca57688431aa4618066ce02a7
Calvin Woo <calvin.woo@citrix.com>
2014-07-09 15:16:52 -0700

    reddit API call on input keypress.

--- app/viewmodels/search.js
+++ app/viewmodels/search.js
@@ -1,5 +1,17 @@
 define(function (require) {
   var ko = require('knockout');
+  var http = require('plugins/http');
 
-  return {};
+  var url = 'http://www.reddit.com/search.json';
+
+  return {
+    query: ko.observable(),
+    results: ko.observableArray(),
+    runSearch: function() {
+      var queryUrl = url + '?q=' + this.query();
+      http.get(queryUrl).then(function(data) {
+        console.log(data.data.children)
+      });
+    }
+  };
 });
