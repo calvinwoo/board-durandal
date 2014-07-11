c21284f8d875a8459d7843a15015121b4b38c7b7
Calvin Woo <calvin.woo@citrix.com>
2014-07-10 17:07:06 -0700

    Fleshed out search junk.

--- app/viewmodels/search.js
+++ app/viewmodels/search.js
@@ -3,17 +3,26 @@ define(function (require) {
   var http = require('plugins/http');
 
   var url = 'http://www.reddit.com/search.json';
+  var ifDisplayNoneText = false;
+
+  var runSearch = function(query, url, assignment) {
+    var searchUrl = url + '?q=' + query();
+    http.get(searchUrl).then(function(data) {
+      ifDisplayNoneText = data.data.children.length;
+      assignment(data.data.children);
+    });
+  }
+
+  var query = ko.observable('').extend({ rateLimit: { timeout: 300, method: 'notifyWhenChangesStop' } });
+  var results = ko.observableArray([]);
+
+  query.subscribe(function() {
+    runSearch(query, url, results);
+  });
 
   return {
-    query: ko.observable(),
-    results: ko.observableArray([]),
-    runSearch: function() {
-      var that = this;
-      var queryUrl = url + '?q=' + this.query();
-      http.get(queryUrl).then(function(data) {
-        that.results(data.data.children);
-        console.log(that.results())
-      });
-    }
+    ifDisplayNoneText: ifDisplayNoneText,
+    query: query,
+    results: results
   };
 });
